bec6a80d0a91f78a1054e8d917b6021c16836319
Gurbir Dhaliwal <curious.gurbir@gmail.com>
2022-07-01 17:49:33 +0530

    Update from G Terminal

--- speakers.js
+++ speakers.js
@@ -1,9 +1,65 @@
-var speakers = [
-	{
-		name: "",
-		title: "",
-		image: "",
-		logo: "",
-		status: "",
-	},
-];
+var speakerData;
+
+$(document).ready(function () {
+	$.ajaxSetup({
+		cache: false,
+	});
+	$.get("data/speakers.csv", "text", function (data) {
+		speakerData = $.csv.toObjects(data);
+		speakerData.map((speaker) => {
+			addCurrentSpeaker(
+				speaker.name,
+				speaker.title,
+				speaker.status,
+				speaker.logo
+			);
+		});
+	});
+});
+
+// function addCurrentSpeaker(name, title, image, logo, status) {
+// 	if (status.toString().toLowerCase() != "true") return;
+// 	$(".speakers-grid").html(
+// 		$(".speakers-grid").html() +
+// 			`<article class="item">
+//                 <img
+//                     class="speaker-image"
+//                     src="${
+// 						image.includes(".jpg") || image.includes(".png")
+// 							? image
+// 							: image + ".jpg"
+// 					}"
+//                     alt="Speaker Image"
+//                 />
+//                 <div class="speakers-text">
+//                     <span class="speaker-name">${name}</span>
+//                     <span class="speaker-title">${title}</span>
+//                 </div>
+//                 <img class="speaker-logo" src="${
+// 					logo.includes(".jpg") || logo.includes(".png")
+// 						? logo
+// 						: logo + ".png"
+// 				}">
+//             </article>`
+// 	);
+// }
+
+function addCurrentSpeaker(name, title, status, logo) {
+	if (status.toString().toLowerCase() != "true") return;
+	let logoElem = `<img class="speaker-logo" src="images/speaker-logos/${name} Logo.png">`;
+	$(".speakers-grid").html(
+		$(".speakers-grid").html() +
+			`<article class="item">
+                <img
+                    class="speaker-image"
+                    src="images/speaker-images/${name}.jpg"
+                    alt="Speaker Image"
+                />
+                <div class="speakers-text">
+                    <span class="speaker-name">${name}</span>
+                    <span class="speaker-title">${title}</span>
+                </div>
+				${logo.toString().toLowerCase() != "true" ? "" : logoElem}
+            </article>`
+	);
+}
