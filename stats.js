3a098b43758d3ae557ccbfc62b0c4eaf7e7b0e58
Gurbir Dhaliwal <curious.gurbir@gmail.com>
2022-07-19 20:36:12 +0530

    Update from G Terminal

--- stats.js
+++ stats.js
@@ -1,24 +1,26 @@
 $(function () {
-	$(".counter").each(function () {
-		var $this = $(this),
-			countTo = $this.attr("data-count");
+	setTimeout(() => {
+		$(".counter").each(function () {
+			var $this = $(this),
+				countTo = $this.attr("data-count");
 
-		$({ countNum: $this.text() }).animate(
-			{
-				countNum: countTo,
-			},
+			$({ countNum: $this.text() }).animate(
+				{
+					countNum: countTo,
+				},
 
-			{
-				duration: 3000,
-				easing: "linear",
-				step: function () {
-					$this.text(Math.floor(this.countNum));
-				},
-				complete: function () {
-					$this.text(this.countNum);
-					//alert('finished');
-				},
-			}
-		);
-	});
+				{
+					duration: 3000,
+					easing: "linear",
+					step: function () {
+						$this.text(Math.floor(this.countNum));
+					},
+					complete: function () {
+						$this.text(this.countNum);
+						//alert('finished');
+					},
+				}
+			);
+		});
+	}, 3000);
 });
