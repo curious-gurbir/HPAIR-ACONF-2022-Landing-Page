06e5e817a5b3b7d0f7b5974b3d9c6d180a3511a1
Gurbir Dhaliwal <curious.gurbir@gmail.com>
2022-07-22 11:30:21 +0530

    Update from G Terminal

--- stats.js
+++ stats.js
@@ -1,26 +1,39 @@
-$(function () {
-	setTimeout(() => {
-		$(".counter").each(function () {
-			var $this = $(this),
-				countTo = $this.attr("data-count");
+let statsAnim = false;
 
-			$({ countNum: $this.text() }).animate(
-				{
-					countNum: countTo,
+function elementInView(elem) {
+	return $(window).scrollTop() < $(elem).offset().top + $(elem).height();
+}
+
+function startStatsCounter() {
+	statsAnim = true;
+	$(".counter").each(function () {
+		var $this = $(this),
+			countTo = $this.attr("data-count");
+
+		$({ countNum: $this.text() }).animate(
+			{
+				countNum: countTo,
+			},
+			{
+				duration: 3000,
+				easing: "linear",
+				step: function () {
+					$this.text(Math.floor(this.countNum));
 				},
-
-				{
-					duration: 3000,
-					easing: "linear",
-					step: function () {
-						$this.text(Math.floor(this.countNum));
-					},
-					complete: function () {
-						$this.text(this.countNum);
-						//alert('finished');
-					},
-				}
-			);
-		});
-	}, 3000);
+				complete: function () {
+					$this.text(this.countNum);
+					//alert('finished');
+				},
+			}
+		);
+	});
+}
+$(function () {
+	if (elementInView($("#stats-container")) && statsAnim == false)
+		startStatsCounter();
+	$(window).scroll(function () {
+		if (elementInView($("#stats-container")) && statsAnim == false)
+			startStatsCounter();
+	});
+	// setTimeout(startStatsCounter, 3000);
 });
